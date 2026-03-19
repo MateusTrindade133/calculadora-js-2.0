84d85f46a0cbed6ce92469246b9c8a42e70a5cd2
MateusTrindade133 <81597306+MateusTrindade133@users.noreply.github.com>
2026-03-19 17:41:41 +0000

    Declaração das constantes das classes result e buttons, junto das variaveis de current number, first operand, operator e restart. Também adição de uma função que atualiza o texto exibido na tela no elemento result

---
+++ script.js
@@ -0,0 +1,19 @@
+const result = document.querySelector(".result");
+/* const declara uma constante, guarda o elemento da classe result dentro da variavel
+document representa toda pagina html carregada
+querySelector(".result") procura o primeiro elemento que tenha a classe result*/
+
+const buttons = document.querySelectorAll(".buttons button"); 
+/* ALL puxa todos os que possuem o elemento selecionado */
+
+let currentNumber = "";
+let firstOperand = null;
+let operator = null;
+let restart = false;
+
+/* let é variavel */
+
+function updateResult(originClear = false) {
+    result.innerText = originClear ? 0 : currentNumber.replace(".", ",");
+}
+
